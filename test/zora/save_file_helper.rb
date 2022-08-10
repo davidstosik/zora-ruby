948d6ee5c4c60095aad0507099925213210ca3c6
David Stosik <david.stosik+git-noreply@gmail.com>
2022-08-10 17:37:55 +0900

    Expose save file paths in test helper

--- test/zora/save_file_helper.rb
+++ test/zora/save_file_helper.rb
@@ -4,7 +4,11 @@ module Zora
   module SaveFileHelper
     def save_file(name)
       @save_files ||= {}
-      @save_files[name] ||= SaveFile.new("test/saves/#{name}.srm")
+      @save_files[name] ||= SaveFile.new(save_file_path(name))
+    end
+
+    def save_file_path(name)
+      "test/saves/#{name}.srm"
     end
   end
 end
